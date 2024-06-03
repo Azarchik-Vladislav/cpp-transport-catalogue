4e0d86cc01f0800398867172717bb3dca4136d47
Azarchik Vladislav <vlad.azarchik@yandex.ru>
2024-06-04 00:14:43 +0500

    Added methods for print of objects status. Removed of "using" from header file

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,5 +1,13 @@
 #include "stat_reader.h"
 
+using namespace std::literals;
+
+using std::ostream;
+using std::optional;
+using std::set;
+using std::string_view;
+using std::vector;
+
 ostream& operator<<(ostream& out, const BusInfo& bus_info) {
     out << "Bus "s << bus_info.bus << ": "s;
     out << bus_info.stops_on_route << " stops on route, "s;
@@ -24,48 +32,56 @@ std::pair<string_view, string_view> ParseQuery(string_view request) {
     return {command_query, name_for_query};
 }
 
+void PrintBusInfo(const optional<BusInfo>& bus_info, string_view name_for_query, 
+                      ostream& output) {
+    if(bus_info.has_value()) {
+        output << bus_info.value() << std::endl;
+    } else {
+        output << "Bus "s << name_for_query << ": "s
+               <<"not found"s << std::endl;
+    }
+}
+
+void PrintStopInfo(const optional<set<string_view>>& stop_info, string_view name_for_query, 
+                   ostream& output) {
+    if(stop_info.has_value()) {
+        output << "Stop "s << name_for_query <<": "s;
+
+        if(stop_info.value().empty()) {
+            output << "no buses"s << std::endl;;
+        } else {
+            output << "buses "s;
+
+            bool is_first = true;
+
+            for(const auto& name_bus : stop_info.value()) {
+                if(is_first) {
+                    output << name_bus;
+                    is_first = false;
+                    continue;
+                }
+                output << " "s  << name_bus;
+            }
+            output << std::endl;
+        } 
+    } else {
+        output << "Stop "s << name_for_query <<": "s;
+        output << "not found"s << std::endl;
+    }
+}
+
 void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, string_view request,
                        std::ostream& output) {
     const auto [command_query, name_for_query] = ParseQuery(request);
 
-    if(command_query == "Bus"s){
+    if(command_query == "Bus"s) {
         auto bus_info = transport_catalogue.GetBusInfo(name_for_query);
-
-        if(bus_info.has_value()){
-            output << bus_info.value() << std::endl;
-        } else {
-            output << "Bus "s << name_for_query << ": "s
-                   <<"not found"s << std::endl;
-        }
+        PrintBusInfo(bus_info, name_for_query, output);
     }
 
     if(command_query == "Stop"s) {
         auto stop_info = transport_catalogue.GetStopInfo(name_for_query);
-
-        if(stop_info.has_value()){
-            output << "Stop "s << name_for_query <<": "s;
-
-            if(stop_info.value().empty()){
-                output << "no buses"s << std::endl;;
-            } else {
-                output << "buses "s;
-
-                bool is_first = true;
-
-                for(const auto& name_bus : stop_info.value()){
-                    if(is_first){
-                        output << name_bus;
-                        is_first = false;
-                        continue;
-                    }
-                    output << " "s  << name_bus;
-                }
-                output << std::endl;
-            } 
-        } else {
-            output << "Stop "s << name_for_query <<": "s;
-            output << "not found"s << std::endl;
-        }
+        PrintStopInfo(stop_info, name_for_query, output);
     }
 
 }
--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -2,21 +2,21 @@
 
 #include <iostream>
 #include <iomanip>
+#include <optional>
 #include <set>
 #include <string_view>
 #include <vector>
 
-
 #include "transport_catalogue.h"
 
-using std::ostream;
-using std::set;
-using std::string_view;
-using std::vector;
+std::ostream& operator<<(std::ostream& out, const BusInfo& bus_info);
 
-ostream& operator<<(ostream& out, const BusInfo& bus_info);
+std::pair<std::string_view, std::string_view> ParseQuery(std::string_view request);
 
-std::pair<string_view, string_view> ParseQuery(string_view request);
+void PrintBusInfo(const std::optional<BusInfo>& bus_info, std::string_view name_for_query, 
+                            std::ostream& output);
+void PrintStopInfo(const std::optional<std::set<std::string_view>>& stop_info, std::string_view name_for_query,
+                    std::ostream& output);
 
 void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
                        std::ostream& output);
